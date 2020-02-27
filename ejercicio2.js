a2ba6324dc2742fa735679229c4c2da37804939b
Maragrm <60449910+Maragrm@users.noreply.github.com>
2020-02-27 21:02:07 +0100

    Add files via upload

---
+++ ejercicio2.js
@@ -0,0 +1,6 @@
+var mensaje1 = "Amigo";
+var mensaje2 = mensaje1.toLowerCase();
+var mensaje3 = mensaje1.replace(mensaje1, mensaje2);
+var mensaje4 = mensaje1.replace("Amigo", "amigo")
+var mensaje5 = mensaje1.split("Amigo").join("amigo");
+
